6dff50a850210fdbc2d5e8c0352def6f27cc1176
blck-snwmn <whitesnowmancreator@gmail.com>
2023-06-08 23:04:14 +0900

    Refactor

--- src/worker.ts
+++ src/worker.ts
@@ -7,14 +7,13 @@ export interface Env {
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { searchParams } = new URL(request.url);
-		let url = searchParams.get("url");
-		let img: Buffer;
+		const url = searchParams.get("url");
 		if (url) {
 			const browser = await puppeteer.launch(env.MYBROWSER);
 			const page = await browser.newPage();
 			await page.setViewport({ width: 1920, height: 1080 });
 			await page.goto(url);
-			img = (await page.screenshot()) as Buffer;
+			const img = (await page.screenshot()) as Buffer;
 			await browser.close();
 			return new Response(img, {
 				headers: {
