69e5572ca276bd3f2ced32403617721a20b660de
blck-snwmn <whitesnowmancreator@gmail.com>
2023-06-08 23:05:57 +0900

    Use early return

--- src/worker.ts
+++ src/worker.ts
@@ -8,22 +8,21 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { searchParams } = new URL(request.url);
 		const url = searchParams.get("url");
-		if (url) {
-			const browser = await puppeteer.launch(env.MYBROWSER);
-			const page = await browser.newPage();
-			await page.setViewport({ width: 1920, height: 1080 });
-			await page.goto(url);
-			const img = (await page.screenshot()) as Buffer;
-			await browser.close();
-			return new Response(img, {
-				headers: {
-					"content-type": "image/jpeg",
-				},
-			});
-		} else {
+		if (!url) {
 			return new Response(
 				"Please add the ?url=https://example.com/ parameter"
 			);
 		}
+		const browser = await puppeteer.launch(env.MYBROWSER);
+		const page = await browser.newPage();
+		await page.setViewport({ width: 1920, height: 1080 });
+		await page.goto(url);
+		const img = (await page.screenshot()) as Buffer;
+		await browser.close();
+		return new Response(img, {
+			headers: {
+				"content-type": "image/jpeg",
+			},
+		});
 	},
 };
